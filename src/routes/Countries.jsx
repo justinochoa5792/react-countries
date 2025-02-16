b440ecc275797710a68db2789ee9c64cdf4ebb35
Justin Ochoa <just.och92@gmail.com>
2025-02-16 10:38:35 -0500

    added UI for countries and filter functionality

--- src/routes/Countries.jsx
+++ src/routes/Countries.jsx
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
 
 const Countries = () => {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+  const [region, setRegion] = useState("");
+
+  const filteredData = countries.filter((country) => {
+    const matched =
+      !search ||
+      country.name.common.toLowerCase().includes(search.toLowerCase());
+
+    const matchedRegion =
+      !region || country.region.toLowerCase().includes(region.toLowerCase());
+
+    return matched && matchedRegion;
+  });
+
+  const getCountry = async () => {
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    const data = await res.json();
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    getCountry();
+  }, []);
+
   return (
-    <div>
-      <h1>Countries</h1>
+    <div className="country-container">
+      <h1 className="title">Countries</h1>
+      <div className="filter-group">
+        <input
+          className="input-field"
+          value={search}
+          type="text"
+          placeholder="Search by name"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <select
+          className="select-field"
+          value={region}
+          onChange={(e) => setRegion(e.target.value)}
+        >
+          <option value="">All Regions</option>
+          <option value="africa">Africa</option>
+          <option value="americas">Americas</option>
+          <option value="asia">Asia</option>
+          <option value="europe">Europe</option>
+          <option value="oceania">Oceania</option>
+        </select>
+      </div>
+      <ul className="country-list">
+        {filteredData.map((country, i) => {
+          return (
+            <li className="country-item" key={i}>
+              <Link
+                className="country-link"
+                to={`/countries/${country.name.common}`}
+              >
+                {country.name.common}
+              </Link>
+              <div className="country-info">
+                Region: {country.region} | Population: {country.population}
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,25 +3,25 @@ import { Link } from "react-router";
 
 const Home = () => {
   return (
-    <div class="page-wrapper">
-      <div class="container">
-        <div class="flex-wrap-container">
-          <div class="column-half column-half-md">
-            <div class="spacing-container spacing-container-lg">
-              <h1 class="heading heading-sm">
-                <span class="block">Explore Countries with </span>
-                <span class="heading-highlight block">Real-Time Data</span>
+    <div className="page-wrapper">
+      <div className="container">
+        <div className="flex-wrap-container">
+          <div className="column-half column-half-md">
+            <div className="spacing-container spacing-container-lg">
+              <h1 className="heading heading-sm">
+                <span className="block">Explore Countries with </span>
+                <span className="heading-highlight block">Real-Time Data</span>
               </h1>
-              <p class="text text-lg">
+              <p className="text text-lg">
                 Discover details about every country around the world – from
                 capitals to regions!
               </p>
-              <div class="button-group button-group-sm button-group-sm-space-x">
-                <Link to="/countries" class="button-primary">
+              <div className="button-group button-group-sm button-group-sm-space-x">
+                <Link to="/countries" className="button-primary">
                   Explore Now
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="w-5 h-5 ml-1"
+                    className="w-5 h-5 ml-1"
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
@@ -33,18 +33,18 @@ const Home = () => {
                     <polyline points="12 5 19 12 12 19"></polyline>
                   </svg>
                 </Link>
-                <Link to="/about" class="button-secondary">
+                <Link to="/about" className="button-secondary">
                   Learn More
                 </Link>
               </div>
             </div>
           </div>
-          <div class="column-half column-half-md">
-            <div class="image-container">
+          <div className="column-half column-half-md">
+            <div className="image-container">
               <img
                 src="https://image.winudf.com/v2/image/Ymx1ZWNhcC5pbWFnZXouYmVhdXRpZnVsX2NvdW50cmllc193YWxscGFwZXJzX3NjcmVlbl8wXzlpMW14OWl1/screen-0.webp?fakeurl=1&type=.webp"
                 alt="Explore countries"
-                class="image"
+                className="image"
               />
             </div>
           </div>
